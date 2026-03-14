5c388680094c346cc39686ad2afe2ccba8b66648
Tu Nombre <209029025+MateoAlRen@users.noreply.github.com>
2026-03-14 17:27:55 -0500

    ALMOST DONE

--- target/generated-sources/annotations/com/employabilityassesment/practice/infrastructure/adapters/out/mapper/TaskMapperImpl.java
+++ target/generated-sources/annotations/com/employabilityassesment/practice/infrastructure/adapters/out/mapper/TaskMapperImpl.java
@@ -1,47 +1,59 @@
 package com.employabilityassesment.practice.infrastructure.adapters.out.mapper;
 
 import com.employabilityassesment.practice.domain.model.Task;
+import com.employabilityassesment.practice.infrastructure.adapters.out.entity.ProjectEntity;
 import com.employabilityassesment.practice.infrastructure.adapters.out.entity.TaskEntity;
+import java.util.UUID;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-12T03:39:16-0500",
+    date = "2026-03-14T17:13:29-0500",
     comments = "version: 1.6.0, compiler: javac, environment: Java 21.0.8 (Amazon.com Inc.)"
 )
 @Component
 public class TaskMapperImpl implements TaskMapper {
 
     @Override
-    public TaskEntity toEntity(Task taskModel) {
-        if ( taskModel == null ) {
+    public TaskEntity toEntity(Task task) {
+        if ( task == null ) {
             return null;
         }
 
         TaskEntity taskEntity = new TaskEntity();
 
-        taskEntity.setTaskId( taskModel.getTaskId() );
-        taskEntity.setTaskTitle( taskModel.getTaskTitle() );
-        taskEntity.setCompleted( taskModel.isCompleted() );
-        taskEntity.setDeleted( taskModel.isDeleted() );
+        taskEntity.setProject( map( task.getProjectId() ) );
+        taskEntity.setTaskId( task.getTaskId() );
+        taskEntity.setTaskTitle( task.getTaskTitle() );
+        taskEntity.setCompleted( task.isCompleted() );
+        taskEntity.setDeleted( task.isDeleted() );
 
         return taskEntity;
     }
 
     @Override
-    public Task toModel(TaskEntity taskEntity) {
-        if ( taskEntity == null ) {
+    public Task toModel(TaskEntity entity) {
+        if ( entity == null ) {
             return null;
         }
 
         Task task = new Task();
 
-        task.setTaskId( taskEntity.getTaskId() );
-        task.setTaskTitle( taskEntity.getTaskTitle() );
-        task.setDeleted( taskEntity.isDeleted() );
-        task.setCompleted( taskEntity.isCompleted() );
+        task.setProjectId( entityProjectProjectId( entity ) );
+        task.setTaskId( entity.getTaskId() );
+        task.setTaskTitle( entity.getTaskTitle() );
+        task.setDeleted( entity.isDeleted() );
+        task.setCompleted( entity.isCompleted() );
 
         return task;
+    }
+
+    private UUID entityProjectProjectId(TaskEntity taskEntity) {
+        ProjectEntity project = taskEntity.getProject();
+        if ( project == null ) {
+            return null;
+        }
+        return project.getProjectId();
     }
 }
